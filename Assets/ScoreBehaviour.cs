b4f563829907aa200fc92fefa78dfb5f27f71a3f
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-05-27 03:23:14 -0300

    Added score behaviour.

---
+++ Assets/ScoreBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreBehaviour : MonoBehaviour
+{
+    TMP_Text textField;
+
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textField.text = "Score: " + score;
+    }
+
+    public void AddScore()
+    {
+        score++;
+    }
+}
--- Assets/Scripts/Player/BallCollision.cs
+++ Assets/Scripts/Player/BallCollision.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BallCollision : MonoBehaviour
 {
+    public UnityEvent OnBallCollision;
+
+    int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,8 @@ public class BallCollision : MonoBehaviour
         if (other.gameObject.tag == "Ball")
         {
             other.gameObject.SetActive(false);
+            score++;
+            OnBallCollision?.Invoke();
         }
     }
 }
