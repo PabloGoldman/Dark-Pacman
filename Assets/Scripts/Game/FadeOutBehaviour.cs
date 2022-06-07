353569bca78b724d7f2dff8aae325a775bf19341
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-06-07 14:55:22 -0300

    Added cupcake material.

--- Assets/Scripts/Game/FadeOutBehaviour.cs
+++ Assets/Scripts/Game/FadeOutBehaviour.cs
@@ -6,7 +6,6 @@ public class FadeOutBehaviour : MonoBehaviour
 {
     MeshRenderer mr;
 
-    [SerializeField] float timeUntilFadedOut = 10;
     // Start is called before the first frame update
     void Start()
     {
