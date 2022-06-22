0048e9d3f2a7658368ec98f46550df4c47f1426f
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-06-21 22:13:34 -0300

    Added mouse rotation behaviour.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    [SerializeField] float groundDrag;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; //Sino el player se cae
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+
+        rb.drag = groundDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(); 
+    }
+
+    void PlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+     
+    void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+}
